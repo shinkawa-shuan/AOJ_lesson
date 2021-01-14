2768fef18a18872fa1b1d12cd31f0d3dbf13a463
Shinkawa Shuan <43779223+suneemon1@users.noreply.github.com>
2021-01-14 18:37:37 +0900

    add

--- aoj_itp_1_5_a.cpp
+++ aoj_itp_1_5_a.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
 #include <cstdio>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int H,W;
-    while(1){
-        cin >> H >> W ;
-        if(H==0&&W==0) break;
-        for(int i=1;i<=H;i++){
-            for(int j=1;j<=W;j++){
-                printf("%c ",'#');
+int main() {
+    int H, W;
+    while (1) {
+        cin >> H >> W;
+        if (H == 0 && W == 0) break;
+        for (int i = 1; i <= H; i++) {
+            for (int j = 1; j <= W; j++) {
+                printf("%c ", '#');
             }
             printf("\n");
         }
